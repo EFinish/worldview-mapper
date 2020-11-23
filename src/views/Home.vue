65c6cdf60e620131b0a3907712416bc2fd5f7235
EFinish <EFinish@gmail.com>
2020-11-23 15:00:43 +0200

    cleaner ui, show stacks on top

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,48 @@
 <template>
    <b-container class="home" fluid>
      <b-row>
-       <b-col md="1">
+       <b-col>
          <b-row>
           <b-col>
-            <strong>Statements</strong>
-            {{ statementStack }}
+            <b-button
+              variant="primary"
+              v-on:click="switchShowStatementStack">
+              Show Statements
+            </b-button>
+            <b-list-group v-if="showStatementStack">
+              <b-list-group-item v-for="statement in statementStack" :key="statement">
+                {{ statement }}
+              </b-list-group-item>
+            </b-list-group>
           </b-col>
-        </b-row>
-        <b-row>
           <b-col>
-            <strong>Premises</strong>
-            {{ premiseStack }}
+            <b-button
+              variant="primary"
+              v-on:click="switchShowPremiseStack">
+              Show Premises
+            </b-button>
+            <b-list-group v-if="showPremiseStack">
+              <b-list-group-item v-for="premise in premiseStack" :key="premise">
+                {{ premise }}
+              </b-list-group-item>
+            </b-list-group>
           </b-col>
-        </b-row>
-        <b-row>
           <b-col>
-            <strong>Args</strong>
-            {{ argumentStack }}
+            <b-button
+              variant="primary"
+              v-on:click="switchShowArgumentStack">
+              Show Arguments
+            </b-button>
+            <b-list-group v-if="showArgumentStack">
+              <b-list-group-item v-for="argument in argumentStack" :key="argument">
+                {{ argument }}
+              </b-list-group-item>
+            </b-list-group>
           </b-col>
         </b-row>
        </b-col>
+     </b-row>
+     <b-row>
         <b-col>
           <b-row>
             <b-col>
@@ -56,10 +78,28 @@ import { State } from 'vuex-class';
   },
 })
 export default class Home extends Vue {
+  showStatementStack = false;
+
+  showPremiseStack = false;
+
+  showArgumentStack = false;
+
   @State('statementStack') statementStack: any
 
   @State('premiseStack') premiseStack: any
 
   @State('argumentStack') argumentStack: any
+
+  switchShowStatementStack() {
+    this.showStatementStack = !this.showStatementStack;
+  }
+
+  switchShowPremiseStack() {
+    this.showPremiseStack = !this.showPremiseStack;
+  }
+
+  switchShowArgumentStack() {
+    this.showArgumentStack = !this.showArgumentStack;
+  }
 }
 </script>
