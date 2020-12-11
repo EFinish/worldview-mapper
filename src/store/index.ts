c2fc23c810ec351a3393c8cd0837a4c478ec096b
EFinish <EFinish@gmail.com>
2020-12-11 12:45:55 +0100

    init example invalid arg

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,9 +2,11 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import {
-  Statement, Premise, Argument, PremiseType,
+  Statement, Premise, Argument,
 } from '@/models';
 import constants from '@/utils/constants';
+
+const premiseTypes = constants.PremiseTypes;
 
 Vue.use(Vuex);
 
@@ -51,7 +53,7 @@ export default new Vuex.Store({
       context.commit('ADD_TO_STATEMENT_STACK', { text: 'I will eat chocolate' } as Statement);
       context.commit('ADD_TO_PREMISE_STACK',
         {
-          type: constants.PremiseTypes.premiseTypeIfThen,
+          type: premiseTypes.premiseTypeIfThen,
           statements: [
             context.getters.getStatementFromStackById(0),
             context.getters.getStatementFromStackById(1),
@@ -59,16 +61,23 @@ export default new Vuex.Store({
         } as Premise);
       context.commit('ADD_TO_PREMISE_STACK',
         {
-          type: constants.PremiseTypes.premiseTypeTrue,
+          type: premiseTypes.premiseTypeTrue,
           statements: [
             context.getters.getStatementFromStackById(0),
           ],
         } as Premise);
       context.commit('ADD_TO_PREMISE_STACK',
         {
-          type: constants.PremiseTypes.premiseTypeTrue,
+          type: premiseTypes.premiseTypeTrue,
           statements: [
             context.getters.getStatementFromStackById(1),
+          ],
+        } as Premise);
+      context.commit('ADD_TO_PREMISE_STACK',
+        {
+          type: premiseTypes.premiseTypeFalse,
+          statements: [
+            context.getters.getStatementFromStackById(0),
           ],
         } as Premise);
       context.commit('ADD_TO_ARGUMENT_STACK',
@@ -78,6 +87,14 @@ export default new Vuex.Store({
             context.getters.getPremiseFromStackById(1),
           ],
           conclusion: context.getters.getPremiseFromStackById(2),
+        });
+      context.commit('ADD_TO_ARGUMENT_STACK',
+        {
+          premises: [
+            context.getters.getPremiseFromStackById(1),
+            context.getters.getPremiseFromStackById(3),
+          ],
+          conclusion: context.getters.getPremiseFromStackById(3),
         });
     },
   },
